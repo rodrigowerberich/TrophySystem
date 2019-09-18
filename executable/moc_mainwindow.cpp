4175f0cf0a8c61ed4acedd04c0188be1605c8685
Rodrigo Werberich <rodrigowerberich@hotmail.com>
2019-09-17 22:45:18 -0300

    Things needed for executable

---
+++ executable/moc_mainwindow.cpp
@@ -0,0 +1,291 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.13.1)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../TrophySystemDemo/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.13.1. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[32];
+    char stringdata0[544];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 9), // "idChanged"
+QT_MOC_LITERAL(2, 21, 0), // ""
+QT_MOC_LITERAL(3, 22, 5), // "newId"
+QT_MOC_LITERAL(4, 28, 21), // "coinsCollectedChanged"
+QT_MOC_LITERAL(5, 50, 8), // "newValue"
+QT_MOC_LITERAL(6, 59, 13), // "deathsChanged"
+QT_MOC_LITERAL(7, 73, 20), // "killedTurtlesChanged"
+QT_MOC_LITERAL(8, 94, 20), // "killedBowsersChanged"
+QT_MOC_LITERAL(9, 115, 28), // "setSliderCoinCollectedAmount"
+QT_MOC_LITERAL(10, 144, 25), // "sliderCoinCollectedAmount"
+QT_MOC_LITERAL(11, 170, 21), // "setSliderDeathsAmount"
+QT_MOC_LITERAL(12, 192, 18), // "sliderDeathsAmount"
+QT_MOC_LITERAL(13, 211, 22), // "setSliderTurtlesAmount"
+QT_MOC_LITERAL(14, 234, 19), // "sliderTurtlesAmount"
+QT_MOC_LITERAL(15, 254, 21), // "setSliderBowsersAmout"
+QT_MOC_LITERAL(16, 276, 18), // "sliderBowsersAmout"
+QT_MOC_LITERAL(17, 295, 5), // "setId"
+QT_MOC_LITERAL(18, 301, 2), // "id"
+QT_MOC_LITERAL(19, 304, 22), // "setCoinCollectedAmount"
+QT_MOC_LITERAL(20, 327, 19), // "coinCollectedAmount"
+QT_MOC_LITERAL(21, 347, 15), // "setDeathsAmount"
+QT_MOC_LITERAL(22, 363, 12), // "deathsAmount"
+QT_MOC_LITERAL(23, 376, 16), // "setTurtlesAmount"
+QT_MOC_LITERAL(24, 393, 13), // "turtlesAmount"
+QT_MOC_LITERAL(25, 407, 15), // "setBowsersAmout"
+QT_MOC_LITERAL(26, 423, 12), // "bowsersAmout"
+QT_MOC_LITERAL(27, 436, 22), // "earnCoinsButtonPressed"
+QT_MOC_LITERAL(28, 459, 16), // "dieButtonPressed"
+QT_MOC_LITERAL(29, 476, 24), // "killTurtlesButtonPressed"
+QT_MOC_LITERAL(30, 501, 24), // "killBowsersButtonPressed"
+QT_MOC_LITERAL(31, 526, 17) // "exitButtonPressed"
+
+    },
+    "MainWindow\0idChanged\0\0newId\0"
+    "coinsCollectedChanged\0newValue\0"
+    "deathsChanged\0killedTurtlesChanged\0"
+    "killedBowsersChanged\0setSliderCoinCollectedAmount\0"
+    "sliderCoinCollectedAmount\0"
+    "setSliderDeathsAmount\0sliderDeathsAmount\0"
+    "setSliderTurtlesAmount\0sliderTurtlesAmount\0"
+    "setSliderBowsersAmout\0sliderBowsersAmout\0"
+    "setId\0id\0setCoinCollectedAmount\0"
+    "coinCollectedAmount\0setDeathsAmount\0"
+    "deathsAmount\0setTurtlesAmount\0"
+    "turtlesAmount\0setBowsersAmout\0"
+    "bowsersAmout\0earnCoinsButtonPressed\0"
+    "dieButtonPressed\0killTurtlesButtonPressed\0"
+    "killBowsersButtonPressed\0exitButtonPressed"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      19,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       5,       // signalCount
+
+ // signals: name, argc, parameters, tag, flags
+       1,    1,  109,    2, 0x06 /* Public */,
+       4,    1,  112,    2, 0x06 /* Public */,
+       6,    1,  115,    2, 0x06 /* Public */,
+       7,    1,  118,    2, 0x06 /* Public */,
+       8,    1,  121,    2, 0x06 /* Public */,
+
+ // slots: name, argc, parameters, tag, flags
+       9,    1,  124,    2, 0x0a /* Public */,
+      11,    1,  127,    2, 0x0a /* Public */,
+      13,    1,  130,    2, 0x0a /* Public */,
+      15,    1,  133,    2, 0x0a /* Public */,
+      17,    1,  136,    2, 0x0a /* Public */,
+      19,    1,  139,    2, 0x0a /* Public */,
+      21,    1,  142,    2, 0x0a /* Public */,
+      23,    1,  145,    2, 0x0a /* Public */,
+      25,    1,  148,    2, 0x0a /* Public */,
+      27,    0,  151,    2, 0x0a /* Public */,
+      28,    0,  152,    2, 0x0a /* Public */,
+      29,    0,  153,    2, 0x0a /* Public */,
+      30,    0,  154,    2, 0x0a /* Public */,
+      31,    0,  155,    2, 0x0a /* Public */,
+
+ // signals: parameters
+    QMetaType::Void, QMetaType::Int,    3,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+    QMetaType::Void, QMetaType::Int,    5,
+
+ // slots: parameters
+    QMetaType::Void, QMetaType::Int,   10,
+    QMetaType::Void, QMetaType::Int,   12,
+    QMetaType::Void, QMetaType::Int,   14,
+    QMetaType::Void, QMetaType::Int,   16,
+    QMetaType::Void, QMetaType::Int,   18,
+    QMetaType::Void, QMetaType::Int,   20,
+    QMetaType::Void, QMetaType::Int,   22,
+    QMetaType::Void, QMetaType::Int,   24,
+    QMetaType::Void, QMetaType::Int,   26,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->idChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 1: _t->coinsCollectedChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 2: _t->deathsChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 3: _t->killedTurtlesChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 4: _t->killedBowsersChanged((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 5: _t->setSliderCoinCollectedAmount((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 6: _t->setSliderDeathsAmount((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 7: _t->setSliderTurtlesAmount((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 8: _t->setSliderBowsersAmout((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->setId((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 10: _t->setCoinCollectedAmount((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 11: _t->setDeathsAmount((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 12: _t->setTurtlesAmount((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 13: _t->setBowsersAmout((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 14: _t->earnCoinsButtonPressed(); break;
+        case 15: _t->dieButtonPressed(); break;
+        case 16: _t->killTurtlesButtonPressed(); break;
+        case 17: _t->killBowsersButtonPressed(); break;
+        case 18: _t->exitButtonPressed(); break;
+        default: ;
+        }
+    } else if (_c == QMetaObject::IndexOfMethod) {
+        int *result = reinterpret_cast<int *>(_a[0]);
+        {
+            using _t = void (MainWindow::*)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::idChanged)) {
+                *result = 0;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::coinsCollectedChanged)) {
+                *result = 1;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::deathsChanged)) {
+                *result = 2;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::killedTurtlesChanged)) {
+                *result = 3;
+                return;
+            }
+        }
+        {
+            using _t = void (MainWindow::*)(int );
+            if (*reinterpret_cast<_t *>(_a[1]) == static_cast<_t>(&MainWindow::killedBowsersChanged)) {
+                *result = 4;
+                return;
+            }
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    &QMainWindow::staticMetaObject,
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 19)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 19;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 19)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 19;
+    }
+    return _id;
+}
+
+// SIGNAL 0
+void MainWindow::idChanged(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 0, _a);
+}
+
+// SIGNAL 1
+void MainWindow::coinsCollectedChanged(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 1, _a);
+}
+
+// SIGNAL 2
+void MainWindow::deathsChanged(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 2, _a);
+}
+
+// SIGNAL 3
+void MainWindow::killedTurtlesChanged(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 3, _a);
+}
+
+// SIGNAL 4
+void MainWindow::killedBowsersChanged(int _t1)
+{
+    void *_a[] = { nullptr, const_cast<void*>(reinterpret_cast<const void*>(std::addressof(_t1))) };
+    QMetaObject::activate(this, &staticMetaObject, 4, _a);
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
